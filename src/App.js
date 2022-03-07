fb785d0d7329d9355ed73f30295d677d1512d7e2
giorgi <giorgi.doreuli3721@gmail.com>
2022-03-07 19:52:14 +0400

    import starter page component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import StarterPage from './components/StarterPage'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello</h1>
+      <StarterPage />
     </div>
   );
 }
