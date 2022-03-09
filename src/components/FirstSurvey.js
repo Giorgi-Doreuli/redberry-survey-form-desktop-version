c3a866ad9abf159d50caa68ac42be7aa5db222f0
giorgi <giorgi.doreuli3721@gmail.com>
2022-03-09 11:00:10 +0400

    save

--- src/components/FirstSurvey.js
+++ src/components/FirstSurvey.js
@@ -16,7 +16,7 @@ function FirstSurvey() {
           <h2>hey, rocketeer, what are your coordinates</h2>
       </div>
         <form className="form1">
-            <input required pattern="[a-zA-Z0-4-]+" type="text" placeholder='First Name' onChange={(event) => setFirstName(event.target.value)} value = {FirstName || ''}/>
+            <input required type="text" placeholder='First Name' onChange={(event) => setFirstName(event.target.value)} value = {FirstName || ''}/>
             <input required type="text" placeholder='Last Name'onChange={(event) => setLastName(event.target.value)} value = {LastName || ''}/>
             <input required type="email" placeholder='E Mail'onChange={(event) => setEmail(event.target.value)} value = {Email || ''}/>
             <input required type="text" placeholder='+995 5 _ _ _ _'onChange={(event) => setNumber(event.target.value)} value = {number || ''}/>
